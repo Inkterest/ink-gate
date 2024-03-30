44136374a52cc12559acdd79a87b585981fdc360
IvoRum <ivailo.rumenov@outlook.com>
2024-03-30 23:31:58 +0200

    Created new project fir the aoi gateway

---
+++ ink-gate/src/test/java/com/ink/gate/InkGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ink.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
